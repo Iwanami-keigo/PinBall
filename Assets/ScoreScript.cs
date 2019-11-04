699b8178fc74a424cb587d9079044a5baa32201f
keigoiwanami2 <oresaikou0529@gmail.com>
2019-11-04 20:35:53 +0900

    Lesson5課題提出
    スコア表示およびスマートフォン対応

---
+++ Assets/ScoreScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+
+	private int Score = 0;
+
+	private GameObject scoreText;
+
+	void OnCollisionEnter(Collision other){
+
+		if (other.gameObject.tag == "SmallStarTag") {
+			this.Score += 5;
+		} else if (other.gameObject.tag == "LargeStarTag") {
+			this.Score += 20;
+		} else if (other.gameObject.tag == "SmallCloudTag") {
+			this.Score += 10;
+		} else if (other.gameObject.tag == "LargeCloudTag") {
+			this.Score += 30;
+		}
+	}
+
+
+
+		
+
+	// Use this for initialization
+	void Start () {
+		this.scoreText = GameObject.Find("ScoreText");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.scoreText.GetComponent<Text> ().text = "Score." + Score;
+		
+	}
+
+
+
+}
